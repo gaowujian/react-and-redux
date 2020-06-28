19f387d144bccb3a1e00a92f81cd2cbcffa43100
Tony Gao <gwjlshebei@gmail.com>
2020-06-28 23:28:11 +0800

    封装中间件

--- src/index.js
+++ src/index.js
@@ -6,8 +6,43 @@ import { Provider } from "./react-redux";
 import { createStore } from "./store";
 import { reducer } from "./reducer";
 
+let store = createStore(reducer);
+function logger(store) {
+  let next = store.dispatch;
+  return (action) => {
+    console.log("logger1");
+    let result = next(action);
+    return result;
+  };
+}
+
+function thunk(store) {
+  let next = store.dispatch;
+  return (action) => {
+    console.log("thunk");
+    return typeof action === "function" ? action(store.dispatch) : next(action);
+  };
+}
+
+function logger2(store) {
+  let next = store.dispatch;
+  return (action) => {
+    console.log("logger2");
+    let result = next(action);
+    return result;
+  };
+}
+
+function applyMiddleware(store, middlewares) {
+  middlewares = [...middlewares];
+  middlewares.reverse();
+  middlewares.forEach((middleware) => (store.dispatch = middleware(store)));
+}
+
+applyMiddleware(store, [logger, thunk, logger2]);
+
 ReactDOM.render(
-  <Provider store={createStore(reducer)}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
